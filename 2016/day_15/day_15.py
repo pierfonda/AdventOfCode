82f6a526b37982ba84a0ba96df32d686e959611c
pier <pierfonda@gmail.com>
2021-08-17 15:33:32 +0200

    Cleanup

--- 2016/day_15/day_15.py
+++ 2016/day_15/day_15.py
@@ -1,42 +1,32 @@
-import hashlib
+import re
 
-puzzle_input="ahsbgdzn"
-#puzzle_input="abc"
+with open("input.dat") as file:
+    data = file.read().splitlines()
+    
+machines=[]
 
-keys=[]
-otp_found=0
-otp_max=64
-t_max=1
-i=0
+#Structure of a disc info: [Disc #,npos,t0,pos0] 
 
-def get_hash(string,n):
-    tmphas=(hashlib.md5(string.encode())).hexdigest()
-    for j in range(n):
-        tmphas=(hashlib.md5(tmphas.encode())).hexdigest()
-    return tmphas
+for line in data:
+    discd=re.split(" |#|=|\.|\,",line)
+    machines.append([int(discd[2]),int(discd[4]),int(discd[8]),int(discd[14])])
 
+machines.append([len(machines)+1,11,0,0])
 
-while otp_found<otp_max:
-    i+=1
-#    hexmd5i=(hashlib.md5((puzzle_input+str(i)).encode())).hexdigest()
-    hexmd5i=get_hash(puzzle_input+str(i),2016)
-    t_cur=0
-    for j in range(len(hexmd5i)-2):
-        if hexmd5i[j:j+3]==3*hexmd5i[j] and t_cur<t_max:
-            t_cur+=1
-            t_isgood=False
-            for k in range(1000):
-#                hexmd5k=(hashlib.md5((puzzle_input+str(i+k+1)).encode())).hexdigest()
-                hexmd5k=get_hash(puzzle_input+str(i+k+1),2016)
-                for l in range(len(hexmd5i)-4):
-                    if hexmd5k[l:l+5]==5*hexmd5i[j] and not t_isgood:
-                        if i not in keys:
-                            keys.append(i)
-                            otp_found+=1
-                            t_isgood=True
-                            print(f"Key {i} contains {hexmd5i[j:j+3]} and Key {i+k+1} contains {hexmd5k[l:l+5]}")
-                if t_isgood:
-                    break
+level=0
+in_t=-1
 
-print(keys)
-print(len(keys))
+while level<len(machines):
+    in_t+=1
+    t=in_t
+    level=0
+    for i in range(len(machines)):
+        t+=1
+        if (t+machines[i][2]+machines[i][3])%machines[i][1]==0:
+            level+=1
+        else:
+            break
+
+print(in_t,level)
+    
+
